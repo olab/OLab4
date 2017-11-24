8e73281d636d7623e46d54c0ac4aa93e5c92faf2
Corey Wirun <corey@cardinalcreek.ca>
2017-11-24 13:18:02 -0700

    Fixed problem with cache cleanup deleting .gitignore

--- docker/olab4/code-setup/code-setup.sh
+++ docker/olab4/code-setup/code-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rm -Rf /var/www/vhosts/Olab4/www-root/core/storage/cache
+rm -Rf /var/www/vhosts/OLab4/www-root/core/storage/cache/*
 
-if [ ! -f /var/www/vhosts/Olab4/composer.lock ]; then
-	cd /var/www/vhosts/Olab4
+if [ ! -f /var/www/vhosts/OLab4/composer.lock ]; then
+	cd /var/www/vhosts/OLab4
 	composer update
 fi
