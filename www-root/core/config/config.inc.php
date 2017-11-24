56348420ef68f97cf1cabdd43fe389a1b2852098
Corey Wirun <corey@cardinalcreek.ca>
2017-11-24 12:27:01 -0700

    Updated base config.inc.php to match new data exports

---
+++ www-root/core/config/config.inc.php
@@ -0,0 +1,26 @@
+<?php
+return array (
+  'entrada_url' => 'http://olab4.dev',
+  'entrada_relative' => '',
+  'entrada_absolute' => '/var/www/vhosts/OLab4/www-root',
+  'entrada_storage' => '/var/www/vhosts/OLab4/www-root/core/storage',
+  'database' =>
+  array (
+    'adapter' => 'mysqli',
+    'host' => '127.0.0.1',
+    'username' => 'entrada',
+    'password' => 'password',
+    'entrada_database' => 'entrada',
+    'auth_database' => 'entrada_auth',
+    'clerkship_database' => 'entrada_clerkship',
+    'openlabyrinth_database' => 'openlabyrinth'
+  ),
+  'admin' =>
+  array (
+    'firstname' => 'System',
+    'lastname' => 'Administrator',
+    'email' => 'corey@cardinalcreek.ca',
+  ),
+  'auth_username' => '72b796b567a5bc6071e5bce175af3e8f',
+  'auth_password' => '13acf85c37d73e5c32c3a17675cc3533',
+);
