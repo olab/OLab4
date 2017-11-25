a9c1bfb2be4e4b4dc8c36aea6cd00cbb7b51c9b4
Corey Wirun <corey@cardinalcreek.ca>
2017-11-25 14:05:31 -0700

    Cleanup of old files

--- docker/olab4/code-setup/code-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm -Rf /var/www/vhosts/OLab4/www-root/core/storage/cache/*
-
-if [ ! -f /var/www/vhosts/OLab4/composer.lock ]; then
-	cd /var/www/vhosts/OLab4
-	composer update
-fi
