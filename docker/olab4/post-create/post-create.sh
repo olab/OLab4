bf5ffc60ccf5d39504ea6135e3f6f24a2a3d4623
Corey Wirun <corey@cardinalcreek.ca>
2017-11-25 14:33:02 -0700

    Added autoo download of demo data for dbs

--- docker/olab4/post-create/post-create.sh
+++ docker/olab4/post-create/post-create.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x 
 
 rm -Rf /var/www/vhosts/OLab4/www-root/core/storage/cache/*
 
@@ -9,15 +10,20 @@ if [ ! -f /var/www/vhosts/OLab4/composer.lock ]; then
   touch /tmp/composer.finished
 fi
 
-if [ -f /var/lib/mysql/entrada_data.sql ]; then
-  touch /tmp/entrada_data.start
-	mysql -uroot -ppassword < /var/lib/mysql/entrada_data.sql
-  touch /tmp/entrada_data.finished
+cd /var/lib/mysql
+
+if [ ! -f /var/lib/mysql/entrada_data.sql ]; then  
+  wget http://www.olab.ca:26000/demo/entrada_data.sql
 fi
 
-if [ -f /var/lib/mysql/openlabyrinth_data.sql ]; then
-  touch /tmp/openlabyrinth_data.start
-	mysql -uroot -ppassword < /var/lib/mysql/openlabyrinth_data.sql
-  touch /tmp/openlabyrinth_data.finished
-fi
+touch /tmp/entrada_data.start
+mysql -uroot -ppassword < /var/lib/mysql/entrada_data.sql
+touch /tmp/entrada_data.finished
 
+if [ ! -f /var/lib/mysql/openlabyrinth_data.sql ]; then
+  wget http://www.olab.ca:26000/demo/openlabyrinth_data.sql
+fi 
+
+touch /tmp/openlabyrinth_data.start
+mysql -uroot -ppassword < /var/lib/mysql/openlabyrinth_data.sql
+touch /tmp/openlabyrinth_data.finished
