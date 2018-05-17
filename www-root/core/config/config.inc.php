1dd12dc9fc29a4bf85438a97b395ad4e165b793a
Corey Wirun <corey@cardinalcreek.ca>
2018-05-17 12:28:02 -0600

    added reset data script.  changed database server to hosts entry

--- www-root/core/config/config.inc.php
+++ www-root/core/config/config.inc.php
@@ -10,12 +10,12 @@ return array (
   'database' =>
   array (
     'adapter' => 'mysqli',
-    'host' => 'diskstation',
+    'host' => 'database',
     'username' => 'entrada',
     'password' => 'password',
-    'entrada_database' => 'entrada_api',
-    'auth_database' => 'entrada_api_auth',
-    'clerkship_database' => 'entrada_api_clerkship',
+    'entrada_database' => 'entrada',
+    'auth_database' => 'entrada_auth',
+    'clerkship_database' => 'entrada_clerkship',
     // CMW: added
     'openlabyrinth_database' => 'openlabyrinth'
   ),
