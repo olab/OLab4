0e89becb9fc692d3613b1f223c07f9a1a108f844
Corey Wirun <corey@cardinalcreek.ca>
2018-05-19 12:41:10 -0600

    Changed reset db helper script to take a dump version parameter

--- docker/olab4/mariadb/reset-data-db.sh
+++ docker/olab4/mariadb/reset-data-db.sh
@@ -5,31 +5,31 @@ cd /var/lib/mysql
 echo "Clearing existing schemas"
 mysql -uroot -ppassword < /tmp/reset-data-db.sql
 
-echo "Testing for entrada seed data"
-if [ ! -f /var/lib/mysql/entrada_data.sql ]; then  
-    if [ ! -f /var/lib/mysql/entrada_data.sql.gz ]; then  
-        echo "Retrieving entrada seed data"
-        wget http://www.olab.ca/apidev/demo-files/entrada_data.sql.gz
+echo "Testing for entrada $1 seed data"
+if [ ! -f /var/lib/mysql/entrada_data$1.sql ]; then  
+    if [ ! -f /var/lib/mysql/entrada_data$1.sql.gz ]; then  
+        echo "Retrieving entrada $1 seed data"
+        wget http://www.olab.ca/apidev/demo-files/entrada_data$1.sql.gz
     fi
-    gunzip -v entrada_data.sql.gz
+    gunzip -v entrada_data$1.sql.gz
 fi
 
-echo "Loading entrada seed data"
-touch /tmp/entrada_data.start
-pv /var/lib/mysql/entrada_data.sql | mysql -uroot -ppassword 
-touch /tmp/entrada_data.finished
+echo "Loading entrada $1 seed data"
+touch /tmp/entrada_data$1.start
+pv /var/lib/mysql/entrada_data$1.sql | mysql -uroot -ppassword 
+touch /tmp/entrada_data$1.finished
 
-echo "Testing for OLab seed data"
-if [ ! -f /var/lib/mysql/openlabyrinth_data.sql ]; then
-    if [ ! -f /var/lib/mysql/openlabyrinth_data.sql.gz ]; then
-        echo "Retrieving OLab seed data"
-        wget http://www.olab.ca/apidev/demo-files/openlabyrinth_data.sql.gz
+echo "Testing for OLab $1 seed data"
+if [ ! -f /var/lib/mysql/openlabyrinth_data$1.sql ]; then
+    if [ ! -f /var/lib/mysql/openlabyrinth_data$1.sql.gz ]; then
+        echo "Retrieving OLab $1 seed data"
+        wget http://www.olab.ca/apidev/demo-files/openlabyrinth_data$1.sql.gz
     fi  
-    gunzip -v openlabyrinth_data.sql.gz
+    gunzip -v openlabyrinth_data$1.sql.gz
 fi 
 
-echo "Loading OLab seed data"
-touch /tmp/openlabyrinth_data.start
-pv /var/lib/mysql/openlabyrinth_data.sql | mysql -uroot -ppassword
-touch /tmp/openlabyrinth_data.finished
+echo "Loading OLab $1 seed data"
+touch /tmp/openlabyrinth_data$1.start
+pv /var/lib/mysql/openlabyrinth_data$1.sql | mysql -uroot -ppassword
+touch /tmp/openlabyrinth_data$1.finished
 
