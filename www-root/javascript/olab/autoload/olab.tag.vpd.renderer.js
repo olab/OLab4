d4afc95778296c918e051fb66030281124fa6180
Corey Wirun <corey@cardinalcreek.ca>
2018-08-11 21:03:15 -0600

    Added rudimentary VPD tag support

---
+++ www-root/javascript/olab/autoload/olab.tag.vpd.renderer.js
@@ -0,0 +1,57 @@
+﻿Vue.component('olab-vpd',
+{
+    template:`<table rules="NONE" frame="BOX" width="100%" cellspacing="0" cellpadding="6" border="1">
+                <tbody>
+                    <tr>
+                        <td width="30%" valign="top" align="left"><p><strong>Patient Data</strong></p></td>
+                        <td valign="top" align="left"><p>Alice Bond : Name</p></td>
+                    </tr>
+                </tbody>
+              </table>`,
+    props:['src']
+});
+
+var OlabVPDTag = function(olabNodePlayer) {
+
+  var vm = this;
+  vm.olabNodePlayer = olabNodePlayer;
+  vm.OLAB_HTML_TAG = "olab-vpd";
+
+  var service = {
+    render:render
+  };
+
+  return service;
+
+  function render(wikiTagParts) {
+
+    var element = "";
+
+    try {
+
+      var id = wikiTagParts[2];
+      var tag = vm.OLAB_HTML_TAG;
+
+      element = "<" + tag + " class='" + tag + "'></" + tag + ">";
+
+      vm.olabNodePlayer.log.debug(element);
+
+    } catch (e) {
+      element = "[[" + wikiTagParts.join("") + " ERROR: '" + e.message + "']]";
+      vm.olabNodePlayer.log.error(element);
+    }
+
+    return element;
+  }
+
+}
+
+jQuery(window).ready(function ($) {
+
+  try {
+    //alert("OlabCRTag");
+  } catch (e) {
+    alert(e.message);
+  }
+
+});
