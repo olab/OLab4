bacaeb1b4d20a3c4c477b083bba5b1396a77403d
Corey Wirun <corey@cardinalcreek.ca>
2018-10-05 12:37:17 -0600

    Fixed bugs with annotations

--- www-root/core/modules/public/olab/play.inc.php
+++ www-root/core/modules/public/olab/play.inc.php
@@ -65,7 +65,7 @@ if((!defined("PARENT_INCLUDED")) || (!defined("IN_OLAB"))) {
 <!-- DIVs for Olab content binding.  DO NOT EDIT. -->
 <div id="olabHeaderContent" align="center"></div>
 <div id="olabNodeContent" align="center"></div>
-<div id="olabAnnotationContent" class="" align="center"></div>
+<div id="olabAnnotationContent" class=""></div>
 <div id="olabFooterContent" align="center"></div>
 
 
