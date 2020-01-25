49034ad5106084f39841371c47e719f640bc14b0
Corey Wirun <corey@cardinalcreek.ca>
2020-01-25 11:16:14 -0700

    added common function to post_create script

--- docker/Setup/post-create/post-create.sh
+++ docker/Setup/post-create/post-create.sh
@@ -23,30 +23,25 @@ fi
 
 cd /var/lib/mysql
 
-echo "Testing for entrada seed data"
-if [ ! -f /var/lib/mysql/entrada_data.sql ]; then  
-    if [ ! -f /var/lib/mysql/entrada_data.sql.gz ]; then  
-        echo "Retrieving entrada seed data"
-        wget http://demo.olab.ca/demo-files/entrada_data.sql.gz
-    fi
-    gunzip -v entrada_data.sql.gz
-fi
+loadscript() {
 
-echo "Loading entrada seed data"
-touch /tmp/entrada_data.start
-mysql -uroot -ppassword < /var/lib/mysql/entrada_data.sql
-touch /tmp/entrada_data.finished
+	echo "Testing for $1"
+	if [ ! -f /var/lib/mysql/$1 ]; then  
+		if [ ! -f /var/lib/mysql/$1.gz ]; then  
+			echo "Retrieving $1"
+			wget http://demo.olab.ca/demo-files/$1.gz
+		fi
+		gunzip -v $1.gz
+	fi
 
-echo "Testing for OLab seed data"
-if [ ! -f /var/lib/mysql/openlabyrinth_data.sql ]; then
-    if [ ! -f /var/lib/mysql/openlabyrinth_data.sql.gz ]; then
-        echo "Retrieving OLab seed data"
-        wget http://demo.olab.ca/demo-files/openlabyrinth_data.sql.gz
-    fi  
-    gunzip -v openlabyrinth_data.sql.gz
-fi 
+	echo "Loading $1"
+	touch /tmp/$1.start
+	mysql -uroot -ppassword < /var/lib/mysql/$1
+	touch /tmp/$1.finished
 
-echo "Loading OLab seed data"
-touch /tmp/openlabyrinth_data.start
-mysql -uroot -ppassword < /var/lib/mysql/openlabyrinth_data.sql
-touch /tmp/openlabyrinth_data.finished
+}
+
+loadscript "entrada_auth_data.sql"
+loadscript "entrada_clerkship_data.sql"
+loadscript "entrada_data.sql"
+loadscript "openlabyrinth_data.sql"
---
+++ www-root/javascript/olab/autoload/olab.tag.url.renderer.js
@@ -0,0 +1,28 @@
+﻿var OlabURLTag = function (olabNodePlayer) {
+
+  var vm = this;
+  vm.olabNodePlayer = olabNodePlayer;
+
+  var service = {
+    render: render
+  };
+
+  return service;
+
+  function render(wikiTagParts) {
+
+    var element = "";
+    return element;
+  }
+
+};
+
+jQuery(window).ready(function ($) {
+
+  try {
+    //alert("OlabCRTag");
+  } catch (e) {
+    alert(e.message);
+  }
+
+});
