ae9689ab656e94095f409ccb8f39b0b7de594992
Corey Wirun <corey.wirun@itcsmpihpes02.ucalgary.ca>
2020-01-26 12:19:41 -0700

    repo combine complete

--- OLab4-site/www-root/core/config/config.inc.php
+++ OLab4-site/www-root/core/config/config.inc.php
@@ -1,35 +1,33 @@
 <?php
-
 return array (
-  'entrada_url' => 'http://olab4.localhost/apidev',
-  'entrada_relative' => '/apidev',
-  'entrada_absolute' => '/var/www/vhosts/OLab4/www-root',
+  'entrada_url' => 'https://dev.olab.ca/player',
+  'entrada_relative' => '/player',
+  'entrada_absolute' => '/var/www/vhosts/OLab.dev/player/OLab4-site/www-root',
    // CMW: added
-  'entrada_api_absolute' => '/var/www/vhosts/OLab4-api',
-  'entrada_storage' =>      '/var/www/vhosts/OLab4/www-root/core/storage',
-  'database' =>
-    array (
-      'adapter' => 'mysqli',
-      'host' => 'database',
-      'username' => 'entrada',
-      'password' => 'password',
-      'entrada_database' => 'entrada',
-      'auth_database' => 'entrada_auth',
-      'clerkship_database' => 'entrada_clerkship',
-      // CMW: added
-      'openlabyrinth_database' => 'openlabyrinth'
-    ),
-  'service_account' => 
+  'entrada_api_absolute' => '/var/www/vhosts/OLab.dev/player/OLab4-api',
+  'entrada_storage' => '/var/www/vhosts/OLab.dev/player/OLab4-site/www-root/core/storage',
+  'database' => array (
+    'adapter' => 'mysqli',
+    'host' => '127.0.0.1',
+    'username' => 'entrada_dev',
+    'password' => 'b012db9d53',
+    'entrada_database' => 'entrada_dev',
+    'auth_database' => 'entrada_auth_dev',
+    'clerkship_database' => 'entrada_clerkship_dev',
+    // CMW: added
+    'openlabyrinth_database' => 'openlabyrinth_dev'
+  ),
+    'service_account' => 
     array( 
       'username' => 'service', 
       'password' => 'account001'
     ),
   'admin' =>
-    array (
-      'firstname' => 'System',
-      'lastname' => 'Administrator',
-      'email' => 'corey@cardinalcreek.ca',
-    ),
+  array (
+    'firstname' => 'System',
+    'lastname' => 'Administrator',
+    'email' => 'corey@cardinalcreek.ca',
+  ),
   'auth_username' => '63a9de5adfe453481679d421f490222e',
   'auth_password' => '4a7f94f8938f6e5549d9a1b65a591205',
 );
