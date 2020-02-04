3a0a219e95b229315871ad62ab3c73eefae194af
Corey Wirun <corey@cardinalcreek.ca>
2020-02-04 09:17:20 -0700

    Cleanup of individual stage file directories

--- OLab4-site/docker/Setup/mariadb/create-olab-db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `openlabyrinth` CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON `openlabyrinth`.* TO 'entrada'@'%';
-GRANT ALL ON `openlabyrinth`.* TO 'entrada'@'localhost';
-FLUSH PRIVILEGES;
-
---SOURCE /var/lib/mysql/entrada_data.sql;
---SOURCE /var/lib/mysql/openlabyrinth_data.sql;
-
--- OLab4-site/docker/Setup/mariadb/dump-data-db.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#set -x 
-echo "Dumping entrada schemas"
-mysqldump -uroot -ppassword -h database --databases entrada entrada_auth entrada_clerkship > /tmp/entrada_data.sql
-
-echo "Dumping OLab schema"
-mysqldump -uroot -ppassword -h database --databases openlabyrinth > /tmp/openlabyrinth_data.sql
